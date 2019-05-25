c59b5b4cfc6fe93691ea3564772f90a775efab45
htchan <jacky19980619@gmail.com>
2019-05-25 21:43:45 +0800

    create folder for sub project

---
+++ Books/ClassDefinition.py
@@ -0,0 +1,8 @@
+class Book():
+    def __init__(self, name='', writer='', date='', chapter='', website='', bookType=''):
+        self.name = name
+        self.writer = writer
+        self.date = date
+        self.chapter = chapter
+        self.website = website
+        self.bookType = bookType
--- ClassDefinition.py
+++
@@ -1,8 +0,0 @@
-class Book():
-    def __init__(self, name='', writer='', date='', chapter='', website='', bookType=''):
-        self.name = name
-        self.writer = writer
-        self.date = date
-        self.chapter = chapter
-        self.website = website
-        self.bookType = bookType
