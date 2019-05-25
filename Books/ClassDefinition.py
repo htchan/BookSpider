07e385c3afe08fc1e3f2b4ee8cdaa550a0bbc715
htchan <jacky19980619@gmail.com>
2019-05-25 22:17:09 +0800

    Define base class of book

--- Books/ClassDefinition.py
+++ Books/ClassDefinition.py
@@ -6,3 +6,23 @@ class Book():
         self.chapter = chapter
         self.website = website
         self.bookType = bookType
+
+
+class BaseBook():
+    def __init__(self, web):
+        self._website = web
+        self._name = ""
+        self._writer = ""
+        self._date = ""     # last update date
+        self._chapter = ""  # last chapter
+        self._bookType = ""
+        self._chapterSet = []
+        self._downloadAddr = ""
+        self._text = ""
+        self._getBasicInfo()
+    def _getBasicInfo():
+        pass
+    def DownloadBook():
+        pass
+    def Save():
+        pass
