b2bee328ab4a97ef187f81ce7b7b0f268fcaef6f
htchan <htchanbc@connect.ust.hk>
2022-02-07 21:02:17 +0800

    update testcase of pkg/configs

--- backend/pkg/configs/config_test.go
+++ backend/pkg/configs/config_test.go
@@ -5,33 +5,36 @@ import (
 	"os"
 )
 
-func Test_Config_Config_LoadConfigYaml(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		config := LoadConfigYaml(os.Getenv("ASSETS_LOCATION") + "/test-data/config.yml")
+func Test_Config_Config(t *testing.T) {
+	t.Run("func LoadConfigYaml", func(t *testing.T) {
+		t.Run("has correct data", func(t *testing.T) {
+			t.Parallel()
+			config := LoadConfigYaml(os.Getenv("ASSETS_LOCATION") + "/test-data/config.yml")
 
-		if config.Backend.StageFile != "/log/stage.txt" ||
-			config.Backend.LogFile != "/log/controller.log" ||
-			len(config.Backend.Api) != 8 {
-				t.Fatalf("Load wrong backend config %v", config.Backend)
-			}
-		
-		if site, ok := config.SiteConfigs["test"]; !ok ||
-			site.DecoderString != "big5" ||
-			site.BookConfigLocation != "/test-data/book-config.yml" ||
-			site.DatabaseEngine != "sqlite3" ||
-			site.DatabaseLocation != "./test.db" ||
-			site.StorageDirectory != "/test-data/storage/" ||
-			site.ThreadsCount != 1000 || site.ConstSleep != 1000 ||
-			site.MaxExploreError != 1000 ||
-			site.Decoder == nil || site.BookMeta == nil {
-				t.Fatalf("Load wrong site config %v", site)
-			}
-		
-		if bookConfig := config.SiteConfigs["test"].BookMeta;
-			bookConfig.CONST_SLEEP != 1000 ||
-			bookConfig.Decoder == nil ||
-			bookConfig.StorageDirectory != "/test-data/storage/" {
-				t.Fatalf("Load wrong book config %v", bookConfig)
-			}
+			if config.Backend.StageFile != "/log/stage.txt" ||
+				config.Backend.LogFile != "/log/controller.log" ||
+				len(config.Backend.Api) != 8 {
+					t.Fatalf("Load wrong backend config %v", config.Backend)
+				}
+			
+			if site, ok := config.SiteConfigs["test"]; !ok ||
+				site.DecoderString != "big5" ||
+				site.BookConfigLocation != "/test-data/book-config.yml" ||
+				site.DatabaseEngine != "sqlite3" ||
+				site.DatabaseLocation != "./test.db" ||
+				site.StorageDirectory != "/test-data/storage/" ||
+				site.ThreadsCount != 1000 || site.ConstSleep != 1000 ||
+				site.MaxExploreError != 1000 ||
+				site.Decoder == nil || site.BookMeta == nil {
+					t.Fatalf("Load wrong site config %v", site)
+				}
+			
+			if bookConfig := config.SiteConfigs["test"].BookMeta;
+				bookConfig.CONST_SLEEP != 1000 ||
+				bookConfig.Decoder == nil ||
+				bookConfig.StorageDirectory != "/test-data/storage/" {
+					t.Fatalf("Load wrong book config %v", bookConfig)
+				}
+		})
 	})
 }
