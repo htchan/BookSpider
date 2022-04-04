26b76c1726da7291c0fefc0f1a69881ed63c6edc
htchan <jacky19980619@gmail.com>
2022-04-04 23:46:27 +0800

    make book page content selectable

--- frontend/src/lib/Pages/bookPage.dart
+++ frontend/src/lib/Pages/bookPage.dart
@@ -42,12 +42,12 @@ class _BookPageState extends State<BookPage> {
   
   Widget _renderBookContent(Map<String, dynamic> info) {
     List<Widget> rows = [
-      Text('ID: ${info['id']} - ${info['hash']}'),
-      Text('Title: ${info['title']}'),
-      Text('Writer: ${info['writer']}'),
-      Text('Type: ${info['type']}'),
-      Text('Last Update: ${info['updateDate']}'),
-      Text('Last Chapter: ${info['updateChapter']}')
+      SelectableText('ID: ${info['id']} - ${info['hash']}'),
+      SelectableText('Title: ${info['title']}'),
+      SelectableText('Writer: ${info['writer']}'),
+      SelectableText('Type: ${info['type']}'),
+      SelectableText('Last Update: ${info['updateDate']}'),
+      SelectableText('Last Chapter: ${info['updateChapter']}')
     ];
     if (info['status'] == 'download') {
       rows.add(RaisedButton(
