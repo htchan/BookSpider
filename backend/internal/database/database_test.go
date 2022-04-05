2ed587708570b4a83f056b52873c1a6811ee3242
htchan <htchanbc@connect.ust.hk>
2022-04-05 22:39:18 +0800

    [test coverage] database coverage 100

---
+++ backend/internal/database/database_test.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_database(t *testing.T) {
+	t.Run("func GenerateHash", func (t *testing.T) {
+		result := GenerateHash()
+		time.Sleep(1 * time.Second)
+		compareResult := GenerateHash()
+		if compareResult - result < 1000 || compareResult - result > 1005 {
+			t.Fatalf("got difference: %v", compareResult - result)
+		}
+	})
+
+	t.Run("func StatustoString", func (t *testing.T) {
+		t.Run("Error status", func (t *testing.T) {
+			result := StatustoString(Error)
+			if result != "error" {
+				t.Fatalf("got result: %v", result)
+			}
+		})
+
+		t.Run("InProgress status", func (t *testing.T) {
+			result := StatustoString(InProgress)
+			if result != "in_progress" {
+				t.Fatalf("got result: %v", result)
+			}
+		})
+
+		t.Run("End status", func (t *testing.T) {
+			result := StatustoString(End)
+			if result != "end" {
+				t.Fatalf("got result: %v", result)
+			}
+		})
+
+		t.Run("Download status", func (t *testing.T) {
+			result := StatustoString(Download)
+			if result != "download" {
+				t.Fatalf("got result: %v", result)
+			}
+		})
+	})
+}
