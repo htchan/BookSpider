40bded0150854639f711dd00089d068435205e09
htchan <htchanbc@connect.ust.hk>
2023-08-05 12:53:07 +0800

    feat: implement simple client

---
+++ backend/internal/client_v2/simple/config.go
@@ -0,0 +1,12 @@
+package simple
+
+import (
+	"time"
+
+	client "github.com/htchan/BookSpider/internal/client_v2"
+)
+
+type SimpleClientConfig struct {
+	RequestTimeout time.Duration       `yaml:"request_timeout" validate:"min=1s"`
+	DecodeMethod   client.DecodeMethod `yaml:"decode_method" validate:"oneof=gbk big5 utf8"`
+}
---
+++ backend/internal/client_v2/simple/config_test.go
@@ -0,0 +1,56 @@
+package simple
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	client "github.com/htchan/BookSpider/internal/client_v2"
+)
+
+func Test_validate_SimpleConfig(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		conf  SimpleClientConfig
+		valid bool
+	}{
+		{
+			name: "valie config",
+			conf: SimpleClientConfig{
+				RequestTimeout: 1 * time.Second,
+				DecodeMethod:   client.DecodeMethodGBK,
+			},
+			valid: true,
+		},
+		{
+			name: "invalid request timeout",
+			conf: SimpleClientConfig{
+				RequestTimeout: 999 * time.Millisecond,
+				DecodeMethod:   client.DecodeMethodBig5,
+			},
+			valid: false,
+		},
+		{
+			name: "invalid decode method",
+			conf: SimpleClientConfig{
+				RequestTimeout: 1 * time.Second,
+				DecodeMethod:   "invalid",
+			},
+			valid: false,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := validator.New().Struct(test.conf)
+			if test.valid && err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
+		})
+	}
+}
