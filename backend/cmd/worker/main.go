d00b75d40cdf6dd8447c9a77283ee2bc8d0ab099
htchan <htchanbc@connect.ust.hk>
2023-10-21 09:51:48 +0800

    feat: update cmd to load all services

--- backend/cmd/worker/main.go
+++ backend/cmd/worker/main.go
@@ -10,7 +10,12 @@ import (
 	config "github.com/htchan/BookSpider/internal/config_new"
 	repo "github.com/htchan/BookSpider/internal/repo/sqlc"
 	"github.com/htchan/BookSpider/internal/service"
+	"github.com/htchan/BookSpider/internal/vendorservice/baling"
+	"github.com/htchan/BookSpider/internal/vendorservice/bestory"
+	"github.com/htchan/BookSpider/internal/vendorservice/ck101"
 	"github.com/htchan/BookSpider/internal/vendorservice/hjwzw"
+	"github.com/htchan/BookSpider/internal/vendorservice/xbiquge"
+	"github.com/htchan/BookSpider/internal/vendorservice/xqishu"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/sync/semaphore"
@@ -21,10 +26,40 @@ func loadServices(vendors []string, db *sql.DB, conf *config.Config) map[string]
 
 	publicSema := semaphore.NewWeighted(int64(conf.BatchConfig.MaxWorkingThreads))
 
+	if slices.Contains(vendors, baling.Host) {
+		rpo := repo.NewRepo(baling.Host, db)
+
+		result[baling.Host] = baling.NewService(rpo, publicSema, conf.SiteConfigs[baling.Host])
+	}
+
+	if slices.Contains(vendors, bestory.Host) {
+		rpo := repo.NewRepo(bestory.Host, db)
+
+		result[bestory.Host] = bestory.NewService(rpo, publicSema, conf.SiteConfigs[bestory.Host])
+	}
+
+	if slices.Contains(vendors, ck101.Host) {
+		rpo := repo.NewRepo(ck101.Host, db)
+
+		result[ck101.Host] = ck101.NewService(rpo, publicSema, conf.SiteConfigs[ck101.Host])
+	}
+
 	if slices.Contains(vendors, hjwzw.Host) {
 		rpo := repo.NewRepo(hjwzw.Host, db)
 
 		result[hjwzw.Host] = hjwzw.NewService(rpo, publicSema, conf.SiteConfigs[hjwzw.Host])
+	}
+
+	if slices.Contains(vendors, xbiquge.Host) {
+		rpo := repo.NewRepo(xbiquge.Host, db)
+
+		result[xbiquge.Host] = xbiquge.NewService(rpo, publicSema, conf.SiteConfigs[xbiquge.Host])
+	}
+
+	if slices.Contains(vendors, xqishu.Host) {
+		rpo := repo.NewRepo(xqishu.Host, db)
+
+		result[xqishu.Host] = xqishu.NewService(rpo, publicSema, conf.SiteConfigs[xqishu.Host])
 	}
 
 	return result
