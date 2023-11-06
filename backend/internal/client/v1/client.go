78e7dc5032b23251e415ff3fa1d8b259ec6b29b0
htchan <htchanbc@connect.ust.hk>
2023-11-06 19:12:55 +0800

    fix: generate after re-structure client package

--- backend/internal/client/v1/client.go
+++ backend/internal/client/v1/client.go
@@ -1,6 +1,6 @@
 package client
 
-//go:generate mockgen -destination=../mock/client/client.go -package=mockclient . Client
+//go:generate mockgen -destination=../../mock/client/client.go -package=mockclient . Client
 type Client interface {
 	Acquire() error
 	Release()
--- backend/internal/client/v2/interface.go
+++ backend/internal/client/v2/interface.go
@@ -2,7 +2,7 @@ package client
 
 import "context"
 
-//go:generate mockgen -destination=../mock/client/v2/book_client.go -package=mockclient . BookClient
+//go:generate mockgen -destination=../../mock/client/v2/book_client.go -package=mockclient . BookClient
 type BookClient interface {
 	Get(ctx context.Context, url string) (string, error)
 }
