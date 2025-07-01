064b5545eef94c18c89b37899ae58e333ecaa5b9
htchan <jacky19980619@gmail.com>
2025-07-01 18:51:56 +0800

    chore: remove deprecated internal/parser package

--- internal/parse/parser.go
+++
@@ -1,8 +0,0 @@
-package parse
-
-//go:generate go tool mockgen -destination=../mock/parser/parser.go -package=mockparser . Parser
-type Parser interface {
-	ParseBook(html string) (*ParsedBookFields, error)
-	ParseChapterList(html string) (*ParsedChapterList, error)
-	ParseChapter(html string) (*ParsedChapterFields, error)
-}
